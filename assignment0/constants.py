2c5cce70c1515e7418247d3e6e69ec598d8d462e
Aishwarya Tonpe <aishwaryatonpe@Aishwaryas-MacBook-Air.local>
2024-02-06 14:09:58 -0500

    adding test constants

--- assignment0/constants.py
+++ assignment0/constants.py
@@ -27,3 +27,77 @@ class strings:
         "new_line" : "\n",
         "comma" : ","
     }
+
+    test_constants = {
+    "print_status_test_output" :
+        """Traffic Stop|78
+Transfer/Interfacility|72
+Disturbance/Domestic|46
+Sick Person|44
+Alarm|24
+Welfare Check|20
+Check Area|18
+Contact a Subject|18
+Follow Up|18
+MVA With Injuries|16
+Supplement Report|14
+Unconscious/Fainting|14
+Breathing Problems|12
+Chest Pain|12
+Hemorrhage/Lacerations|12
+Traumatic Injury|12
+Falls|10
+Fraud|10
+Harassment / Threats Report|10
+Parking Problem|10
+Public Assist|10
+Trespassing|10
+Animal at Large|8
+Fire Alarm|8
+Hit and Run|8
+Larceny|8
+Motorist Assist|8
+Open Door/Premises Check|8
+Suspicious|8
+Unknown Problem/Man Down|8
+COP Relationships|6
+MVA Non Injury|6
+Missing Person|6
+Warrant Service|6
+Abdominal Pains/Problems|4
+Alarm Holdup/Panic|4
+Animal Trapped|4
+Cardiac Respritory Arrest|4
+Escort/Transport|4
+Found Item|4
+Heart Problems/AICD|4
+Mutual Aid|4
+Noise Complaint|4
+Officer Needed Nature Unk|4
+Overdose/Poisoning|4
+Reckless Driving|4
+Road Rage|4
+Stolen Vehicle|4
+Vandalism|4
+911 Call Nature Unknown|2
+Animal Complaint|2
+Animal Vicious|2
+Assault|2
+Assist Fire|2
+COP DDACTS|2
+Civil Standby|2
+Drug Violation|2
+Extra Patrol|2
+Fire Carbon Monoxide Alarm|2
+Fire Grass|2
+Foot Patrol|2
+Kidnapping|2
+Runaway or Lost Child|2
+Stake Out|2
+Test Call|2
+""",
+    "extract_data_test_url_1" : "tmp/2024-01-01_daily_incident_summary.pdf",
+    "extract_data_test_url_2" : "tmp/2024-01-04_daily_incident_summary.pdf",
+    "extract_data_test_expected_count_1" : 329,
+    "extract_data_test_expected_count_2" : 337,
+    }
--- tests/test_extract_data_from_pdf.py
+++ tests/test_extract_data_from_pdf.py
@@ -3,8 +3,8 @@ from assignment0.main import createdb, connectdb, deletedb, extract_data_from_pd
 from assignment0.constants import strings
 
 @pytest.mark.parametrize("pdf_path, expected_count", [
-    ("tmp/2024-01-01_daily_incident_summary.pdf", 329),
-    ("tmp/2024-01-04_daily_incident_summary.pdf", 337)
+    (strings.test_constants["extract_data_test_url_1"], strings.test_constants["extract_data_test_expected_count_1"]),
+    (strings.test_constants["extract_data_test_url_2"], strings.test_constants["extract_data_test_expected_count_2"])
 ])
 def test_extract_data_from_pdf(pdf_path, expected_count):
     result = extract_data_from_pdf(pdf_path)
