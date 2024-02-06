092b70ee2e78cb6679a6021dcbb3e0d53953eff5
Aishwarya Tonpe <aishwaryatonpe@Aishwaryas-MacBook-Air.local>
2024-02-06 14:15:35 -0500

    minor tweaks

--- tests/test_connectdb.py
+++ tests/test_connectdb.py
@@ -5,4 +5,3 @@ def test_connectdb():
     cur, con = connectdb()
     assert cur is not None
     assert con is not None
-    # Add more assertions if needed
--- tests/test_status.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from assignment0.main import createdb, connectdb, deletedb, print_status
-from assignment0.constants import strings
-
-def test_populatedb():
-    sorted_data = print_status()
-
-
